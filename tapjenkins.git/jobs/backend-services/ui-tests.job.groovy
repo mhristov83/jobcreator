95d37e856e12ecb1a6d92435cee9abaacc22f7e1
unknown <mhristov@MHRISTOV.telerik.com>
2015-02-27 14:33:32 +0200

    no message

--- tapjenkins.git/jobs/backend-services/ui-tests.job.groovy
+++ tapjenkins.git/jobs/backend-services/ui-tests.job.groovy
@@ -3,16 +3,16 @@ import helpers.*
 //Constants
 def defaultBranch = 'master'
 
-def api_keys = [id:'0', suiteTitle:"ApiKeys", suiteName:"ApiKeys"]
-def cloud_code = [id:'1', suiteTitle:"CloudCode", suiteName:"CloudCode"]
-def content_type = [id:'2', suiteTitle:"ContentType", suiteName:"ContentType"]
-def downloads = [id:'3', suiteTitle:"Downloads", suiteName:"Downloads"]
-def files = [id:'4', suiteTitle:"Files", suiteName:"Files"]
-def project = [id:'5', suiteTitle:"Project", suiteName:"Project"]
-def push_notifications = [id:'6', suiteTitle:"PushNotifications", suiteName:"PushNotifications"]
-def responsive_images = [id:'7', suiteTitle:"ResponsiveImages", suiteName:"ResponsiveImages"]
-def time_open_project = [id:'8', suiteTitle:"TimeOpenProject", suiteName:"TimeOpenProject"]
-def user = [id:'9', suiteTitle:"User", suiteName:"User"]
+def api_keys = [id:'0', suiteTitle:"ApiKeys", suiteName:"api_keys_tests"]
+def cloud_code = [id:'1', suiteTitle:"CloudCode", suiteName:"cloud_code_tests"]
+def content_type = [id:'2', suiteTitle:"ContentType", suiteName:"content_type_tests"]
+def downloads = [id:'3', suiteTitle:"Downloads", suiteName:"downloads_tests"]
+def files = [id:'4', suiteTitle:"Files", suiteName:"files_tests"]
+def project = [id:'5', suiteTitle:"Project", suiteName:"project_tests"]
+def push_notifications = [id:'6', suiteTitle:"PushNotifications", suiteName:"push_notification_tests"]
+def responsive_images = [id:'7', suiteTitle:"ResponsiveImages", suiteName:"responsive_images_tests"]
+def time_open_project = [id:'8', suiteTitle:"TimeOpenProject", suiteName:"timeout_open_project_tests"]
+def user = [id:'9', suiteTitle:"User", suiteName:"user_tests"]
 
 def suites = []
 suites.add(api_keys)
@@ -40,7 +40,7 @@ environments.add(LIVE)
 //Create the jobs for the test suites
 for (env in environments) {
   for (suite in suites) {
-    def jobName = env.shortName+'-UI-'+suite.suiteTitle +'Tests'
+    def jobName = env.envName+'-UI-'+suite.suiteTitle +'Tests'
     suiteNamesString = jobName + ',' + suiteNamesString
 
     def suiteJob = null
@@ -60,28 +60,28 @@ for (env in environments) {
       if(env.envName=='UAT') {
        steps {
         batchFile(($/
-          \\telerik.com\resources\TAP\QA\Tools\SikuliSet\tools\QRes\QRes.exe /x:1600 /y:1200
-          set TestRunner="C:\SikuliX\runScript.cmd"
-          set TestList="%WORKSPACE%\UITests\SIKULI\sikuli_tests\api_keys_tests.sikuli"
-          set TestEnv=https://testtap.telerik.com
-          set UserEmail=bsload@telerik.local
-          set apiUrl=testapi.everlive.com/v1/
-          set apiKey=MJsFfcOodnypQC63
-          set masterKey=AGkXJ3j1oMSsMMFn14OA9hNn3Y2SWnI6
-          set MetadataAppId=51e2cce0-6f37-11e4-832a-879d9d59ccef
-          set timeout=60
+ \\telerik.com\resources\TAP\QA\Tools\SikuliSet\tools\QRes\QRes.exe /x:1600 /y:1200
+ set TestRunner="C:\SikuliX\runScript.cmd"
+ set TestList="%WORKSPACE%\UITests\SIKULI\sikuli_tests\'+suite.suiteName+'_tests.sikuli"
+ set TestEnv=https://testtap.telerik.com
+ set UserEmail=bsload@telerik.local
+ set apiUrl=testapi.everlive.com/v1/
+ set apiKey=MJsFfcOodnypQC63
+ set masterKey=AGkXJ3j1oMSsMMFn14OA9hNn3Y2SWnI6
+ set MetadataAppId=51e2cce0-6f37-11e4-832a-879d9d59ccef
+ set timeout=60
 
-          call %TestRunner% -r %TestList%
+ call %TestRunner% -r %TestList%
 
-          findstr /m "errors=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
-          if not %ERRORLEVEL%==0 ( 
-            exit /B 1
-            )
+ findstr /m "errors=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
+ if not %ERRORLEVEL%==0 ( 
+   exit /B 1
+   )
 
-          findstr /m "failures=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
-          if not %ERRORLEVEL%==0 ( 
-            exit /B 1
-            )
+ findstr /m "failures=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
+ if not %ERRORLEVEL%==0 ( 
+   exit /B 1
+   )
           /$))
       }
     }
@@ -91,28 +91,28 @@ for (env in environments) {
     {
       steps {
         batchFile(($/
-          \\telerik.com\resources\TAP\QA\Tools\SikuliSet\tools\QRes\QRes.exe /x:1600 /y:1200
-          set TestRunner="C:\SikuliX\runScript.cmd"
-          set TestList="%WORKSPACE%\UITests\SIKULI\sikuli_tests\api_keys_tests.sikuli"
-          set TestEnv=https://testtap.telerik.com
-          set UserEmail=bsload@telerik.local
-          set apiUrl=testapi.everlive.com/v1/
-          set apiKey=MJsFfcOodnypQC63
-          set masterKey=AGkXJ3j1oMSsMMFn14OA9hNn3Y2SWnI6
-          set MetadataAppId=51e2cce0-6f37-11e4-832a-879d9d59ccef
-          set timeout=60
+\\telerik.com\resources\TAP\QA\Tools\SikuliSet\tools\QRes\QRes.exe /x:1600 /y:1200
+set TestRunner="C:\SikuliX\runScript.cmd"
+set TestList="%WORKSPACE%\UITests\SIKULI\sikuli_tests\'+suite.suiteName+'_tests.sikuli"
+set TestEnv=https://testtap.telerik.com
+set UserEmail=bsload@telerik.local
+set apiUrl=testapi.everlive.com/v1/
+set apiKey=MJsFfcOodnypQC63
+set masterKey=AGkXJ3j1oMSsMMFn14OA9hNn3Y2SWnI6
+set MetadataAppId=51e2cce0-6f37-11e4-832a-879d9d59ccef
+set timeout=60
 
-          call %TestRunner% -r %TestList%
+call %TestRunner% -r %TestList%
 
-          findstr /m "errors=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
-          if not %ERRORLEVEL%==0 ( 
-            exit /B 1
-            )
+findstr /m "errors=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
+if not %ERRORLEVEL%==0 ( 
+  exit /B 1
+  )
 
-          findstr /m "failures=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
-          if not %ERRORLEVEL%==0 ( 
-            exit /B 1
-            )
+findstr /m "failures=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
+if not %ERRORLEVEL%==0 ( 
+  exit /B 1
+  )
           /$))
       }
     }
@@ -122,29 +122,29 @@ for (env in environments) {
     {
       steps{
        batchFile(($/
-        \\telerik.com\resources\TAP\QA\Tools\SikuliSet\tools\QRes\QRes.exe /x:1600 /y:1200
-        set TestRunner="C:\SikuliX\runScript.cmd"
-        set TestList="%WORKSPACE%\UITests\SIKULI\sikuli_tests\api_keys_tests.sikuli"
-        set TestEnv=https://platform.telerik.com
-        set UserEmail=bsload@telerik.local
-        set apiUrl=api.everlive.com/v1/
-        set apiKey=UwoNtYbnakMRNucW
-        set masterKey=BcA4dRTerYa5FsT4qNbCTF1grDbI6RDh
-        set MetadataAppId=4cb190e0-6f36-11e4-b1c9-6de58479113e
-        set timeout=60
+\\telerik.com\resources\TAP\QA\Tools\SikuliSet\tools\QRes\QRes.exe /x:1600 /y:1200
+ set TestRunner="C:\SikuliX\runScript.cmd"
+ set TestList="%WORKSPACE%\UITests\SIKULI\sikuli_tests\'+suite.suiteName+'_tests.sikuli"
+ set TestEnv=https://platform.telerik.com
+ set UserEmail=bsload@telerik.local
+ set apiUrl=api.everlive.com/v1/
+ set apiKey=UwoNtYbnakMRNucW
+ set masterKey=BcA4dRTerYa5FsT4qNbCTF1grDbI6RDh
+ set MetadataAppId=4cb190e0-6f36-11e4-b1c9-6de58479113e
+ set timeout=60
 
-        call %TestRunner% -r %TestList%
+ call %TestRunner% -r %TestList%
 
-        findstr /m "errors=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
-        if not %ERRORLEVEL%==0 ( 
-          exit /B 1
-          )
+ findstr /m "errors=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
+ if not %ERRORLEVEL%==0 ( 
+   exit /B 1
+   )
 
-        findstr /m "failures=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
-        if not %ERRORLEVEL%==0 ( 
-          exit /B 1
-          )
-        /$))
+ findstr /m "failures=\"0\"" %WORKSPACE%\UITests\SIKULI\results\TEST-Report.xml 
+ if not %ERRORLEVEL%==0 ( 
+   exit /B 1
+   )
+                /$))
 
      }
    }
@@ -152,8 +152,44 @@ for (env in environments) {
       archiveJunit 'UITests\\SIKULI\\results\\*.xml'
     }
  }
+
 }
 
+job(type: BuildFlow) {
+      name 'Run_all_tests_for_' + env.envName + '_environment'
+      label "ui-tests-runner"
+    
+    deliveryPipelineConfiguration('Environment - ' + env.envName, 'Run Integration Tests')
+    
+    wrappers {
+         timestamps()
+       }
+    
+    
+  buildFlow("""
+    def results = []
+    
+    ignore(FAILURE){ retry(3) {results[0] = build("${env.shortName}-UI-ApiKeys-Tests")}}
+    ignore(FAILURE){ retry(3) {results[1] = build("${env.shortName}-UI-CloudCode-Tests")}}
+    ignore(FAILURE){ retry(3) {results[2] = build("${env.shortName}-UI-ContentType-Tests")}}
+    ignore(FAILURE){ retry(3) {results[3] = build("${env.shortName}-UI-Downloads-Tests")}}
+    ignore(FAILURE){ retry(3) {results[4] = build("${env.shortName}-UI-Files-Tests")}}
+    ignore(FAILURE){ retry(3) {results[5] = build("${env.shortName}-UI-Project-Tests")}}
+    ignore(FAILURE){ retry(3) {results[6] = build("${env.shortName}-UI-PushNotifications-Tests")}}
+    ignore(FAILURE){ retry(3) {results[7] = build("${env.shortName}-UI-ResponsiveImages-Tests")}}
+    ignore(FAILURE){ retry(3) {results[8] = build("${env.shortName}-UI-TimeOpenProject-Tests")}}
+    ignore(FAILURE){ retry(3) {results[9] = build("${env.shortName}-UI-TimeOpenProject-Tests")}}
+    
+    def finalResult = SUCCESS
+    
+    for (build in results) {
+      finalResult = finalResult.combine(build.result)
+    }
+    
+    build.state.setResult(finalResult)    
+    """)
+     }
+  }
 
 suiteNamesString = ''
 }
@@ -176,7 +212,7 @@ for (env in environments) {
     }
     for (suite in suites) {
       jobs {
-        names(env.shortName+'-UI-Tests-'+suite.suiteTitle)
+        names(env.envName+'-UI-Tests-'+suite.suiteTitle)
         name('Run_all_tests_for_' + env.envName + '_environment')
       }
     }
