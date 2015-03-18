933dea52b47e1f56a1d6f50a26e6c0b21de4b3b8
unknown <mhristov@MHRISTOV.telerik.com>
2015-03-18 12:06:12 +0200

    no message

--- tapjenkins.git/jobs/backend-services/view-creator.groovy
+++ tapjenkins.git/jobs/backend-services/view-creator.groovy
@@ -20,58 +20,64 @@ environments.add(LIVE)
 def typeOfTests =[]
 
 def UI=[id:'0', shortName:'UI', envConfig:'ExternalConfigDev1', envConfigTapInt:'ExternalConfigTapIntDev1']
-def INTEGRATION=[id:'1', shortName:'INT', envConfig:'ExternalConfigDev1', envConfigTapInt:'ExternalConfigTapIntDev1']
+def INTEGRATION=[id:'1', shortName:'Integration', envConfig:'ExternalConfigDev1', envConfigTapInt:'ExternalConfigTapIntDev1']
 
 typeOfTests.add(UI)
 typeOfTests.add(INTEGRATION)
 //Create the views for the different environments (integration, test and live)
 
-for(type in typeOfTests){
-  if (type.shortName=='UI'){
-    for(i=2; i<=4; i++){
-      def env=environments[i]
+view(type:NestedView){
+  name('TESTS-MH')
+  description('This view contains all tests')
+  filterBuildQueue()
+  filterExecutors()
+  for(type in typeOfTests){
+    if (type.shortName=='UI'){
+      for(i=2; i<=4; i++){
+        def env=environments[i]
 
-      view(type:ListView){
-        name(env.shortName+'-'+type.shortName)
-        description('This view contains all jobs for ' + env.envName + ' environment. You can trigger manually each test suite or use the "Run_all_UI_tests_for_' + env.envName + '_environment" job in order to trigger all suites.')
-        filterBuildQueue()
-        filterExecutors()
-        columns {
-          status()
-          weather()
-          name()
-          lastSuccess()
-          lastFailure()
-          lastDuration()
-          buildButton()
-        }
-        jobs{
-          regex("${env.envName}-${type.shortName}-.*")
+        view(type:ListView){
+          name(env.envName+'-'+type.shortName)
+          description('This view contains all jobs for ' + env.envName + ' environment. You can trigger manually each test suite or use the "Run_all_UI_tests_for_' + env.envName + '_environment" job in order to trigger all suites.')
+          filterBuildQueue()
+          filterExecutors()
+          columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+          }
+          jobs{
+            regex("${env.envName}-${type.shortName}-.*")
+          }
         }
       }
     }
-  }
 
-  else{
-    for(env in environments){
-      view(type:ListView){
-        name(env.envName+'-'+type.shortName)
-        description('This view contains all jobs for ' + env.envName + ' environment. You can trigger manually each test suite or use the "Run_all_tests_for_' + env.envName + '_environment" job in order to trigger all suites.')
-        filterBuildQueue()
-        filterExecutors()
-        columns {
-          status()
-          weather()
-          name()
-          lastSuccess()
-          lastFailure()
-          lastDuration()
-          buildButton()
+    else{
+      for(env in environments){
+        view(type:ListView){
+          name(env.envName+'-'+type.shortName)
+          description('This view contains all jobs for ' + env.envName + ' environment. You can trigger manually each test suite or use the "Run_all_tests_for_' + env.envName + '_environment" job in order to trigger all suites.')
+          filterBuildQueue()
+          filterExecutors()
+          columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+          }
+          jobs{
+            regex(".+-${env.shortName}")
+          }
         }
-        jobs{
-          regex(".+-${env.shortName}")
-        }
-      }
-    } 
+      } 
+    }
   }
 }
